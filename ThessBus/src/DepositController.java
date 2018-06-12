b45971a1bbb271a2df17163391026336d1d10cac
dennis_antoniadis <dennis.antoniadis@gmail.com>
2018-06-12 21:57:28 +0300

    DepositController initialzed

--- ThessBus/src/DepositController.java
+++ ThessBus/src/DepositController.java
@@ -1,16 +1,22 @@
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class DepositController extends MainController implements Initializable{
 
+	@FXML private Label balanceLabel;
 	
+	public void OnClicked(ActionEvent e) {
+		System.out.println("Yesssss");
+	}
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
-		
+		balanceLabel.setText(Double.toString((Main.loginUser.getBalance())));
 	}
 
 }
