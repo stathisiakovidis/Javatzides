c9a5472a6c5b2d1b3e3eb8b3245a89081cf764b7
stathisiakovidis <36900404+stathisiakovidis@users.noreply.github.com>
2018-06-18 19:11:10 +0300

    Qr code image show 2

--- ThessBus/src/HistoryController.java
+++ ThessBus/src/HistoryController.java
@@ -97,7 +97,7 @@ public class HistoryController extends MainController implements Initializable {
 						alert.setHeaderText(null);
 						alert.setContentText("Ημερομηνία: " + dateTime.substring(0, 10) + " "
 								+ dateTime.substring(11, 13) + ":" + dateTime.substring(13, 15) + ":"
-								+ dateTime.substring(15, 17) + "\n\n Λεωφορείο: " + ((Ticket) product).getBus());
+								+ dateTime.substring(15, 17));
 						ImageView temp = new ImageView();
 						temp.setImage(QRcode.printQRCode(product.getQR_code()));
 						temp.setFitHeight(200);
