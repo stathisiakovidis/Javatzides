484bd4f9e39b7e6d82b1867d085539994d2d0cc9
DESKTOP-BTEJO9S\Nuxteridas <gkoulax33@gmail.com>
2018-06-20 12:00:44 +0300

    A try to fix history bug

--- ThessBus/src/RegisterController.java
+++ ThessBus/src/RegisterController.java
@@ -89,11 +89,11 @@ public class RegisterController extends MainController implements Initializable 
 																Main.loginUser.getPhoneNum(), Main.loginUser.getPassport(),
 																Main.loginUser.getBalance());
 
-														FileManager.updatePassenger(Main.loginUser, "Users.dat", temp);
-														/*
-														 * FileManager.updateFines(Main.loginUser.getUsername(),
-														 * Main.loginUser.getFines(), "Fines.dat");
-														 */
+														//FileManager.updatePassenger(Main.loginUser, "Users.dat", temp);
+														
+														/*FileManager.updateFines(Main.loginUser.getUsername(), 
+																Main.loginUser.getFines(), "Fines.dat");*/
+														 
 													}
 												} 
 												else
